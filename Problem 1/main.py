6f601d9a66f7c68519b51be135550b3a3fea2e3a
Rohit Kumar Soni <54770248+imro16@users.noreply.github.com>
2024-08-11 21:52:02 +0530

    Delete Problem 1 directory

--- Problem 1/main.py
+++
@@ -1,5 +0,0 @@
-from parse_fixed_width_file import parse_fixed_width_file
-
-field_lengths = [5, 10, 10, 8, 4]  # Example specification for employees.txt
-
-parse_fixed_width_file('employees.txt', field_lengths, 'employees.csv')
