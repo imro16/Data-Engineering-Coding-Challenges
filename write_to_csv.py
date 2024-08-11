2c8c8a659707250b240709c958ad937d98df9672
Rohit Kumar Soni <54770248+imro16@users.noreply.github.com>
2024-08-11 21:36:32 +0530

    Delete write_to_csv.py

--- write_to_csv.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-def write_to_csv(parsed_lines, output_file):
-    with open(output_file, mode='w', newline='') as file:
-        writer = csv.writer(file)
-        for line in parsed_lines:
-            writer.writerow(line)
